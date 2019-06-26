de10550d7a71543c24ab844976ec044ac85e74fe
Junhee Lee <jhlee910609@gmail.com>
2019-06-27 00:39:55 +0900

    completed custom dropdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react'
+import styles from './App.scss';
+import classnames from 'classnames/bind';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const cx = classnames.bind(styles);
 
-export default App;
+export default class App extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isShow: false,
+            title: 'init',
+        }
+    }
+
+    handleLiClicked = (e) => {
+        this.setState({ title: e.target.value, isShow: !this.state.isShow })
+
+    }
+
+    render() {
+        const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        const { isShow } = this.state;
+        return (
+            <div className="box" >
+                <h1>{this.state.title}</h1>
+                <input onClick={() => this.setState({ isShow: !this.state.isShow })} placeholder='년도 선택' readOnly />
+                <div className={cx('list-box', { isSelected: isShow })}>
+                    {
+                        isShow && list.map((item, i) => <li key={i} className={cx('li')} value={item} onClick={this.handleLiClicked}>{item}</li>)
+                    }
+                </div>
+                <h1>hello!</h1>
+            </div>
+        )
+    }
+};
